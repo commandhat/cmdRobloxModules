6c4db153bb0ee919b346ba63e9a9948a9a42a43f
commandhat <commandhat@users.noreply.github.com>
2024-06-04 07:57:25 -0500

    Update discontinuation message
    Add to the warning to not use early commits

--- gildedRadio.lua
+++ gildedRadio.lua
@@ -11,7 +11,7 @@ This means anyone who wants to use Guilded will eventually need to contend with:
 - A total inability to promote services or platforms outside of the Roblox ecosystem ("facebook", "instagram", "tiktok" etc. are all banned words on Roblox)
 - A total inability to fight or resolve anything negative happening to your social status (there is a social stigma surrounding Roblox in professional communities)
 
-If you wish to continue using this module, grab a previous commit.
+If you wish to continue using this module, grab a previous commit, but be aware all early commits are bug-filled and nearly useless due to API changes.
 
 V1 will never happen unless the enforced change to Roblox accounts is completely cancelled.
 
